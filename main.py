9d51df6891ed698205057e54133f54c0c42c6f02
t111-a <abohwt111@gmail.com>
2026-06-13 20:34:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -53,4 +53,4 @@ async def on_message(message):
     if content == "help":
         await message.channel.send("الأوامر: \nش [اسم الأغنية] \np [اسم الأغنية] \nوقف \n@البوت settings")
 
-bot.run('')
+bot.run()
